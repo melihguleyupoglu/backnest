335e58f28c6bc17ffb1cff9cfe8353cbc858d8d9
melihguleyupoglu <melihguleyup@gmail.com>
2025-03-13 13:20:35 +0300

    feat: add jwtService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { LoginUserDto } from 'src/users/dto/loginUserDto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 const prisma = new PrismaClient();
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
   async isAuthenticated(user: LoginUserDto): Promise<boolean | void> {
     try {
       const dbUser = await prisma.user.findUnique({
